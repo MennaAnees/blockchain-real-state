cc5878bd3a2651fe1468adbf16e7e4ab986554f9
Menna <mennaanees193@gmail.com>
2020-02-25 20:58:35 +0200

    test the ERCMintable smart contract

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 // migrating the appropriate contracts
 // var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
+var ERC721MintableComplete = artifacts.require("./CustomERC721Token.sol");
 module.exports = function(deployer) {
   // deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+  // deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(ERC721MintableComplete);
 };
