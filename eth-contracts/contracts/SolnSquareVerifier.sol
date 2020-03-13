c2081470c4f6e1414fa29be177b28f8f5a89dd67
Menna <mennaanees193@gmail.com>
2020-03-14 01:46:55 +0200

    add SolnSquareVerifier contract

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,45 @@
-pragma solidity >=0.4.21 <0.6.0;
+
+pragma solidity ^0.5.0;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
-
+import "../../zokrates/code/square/verifier.sol";
+import "./ERC721Mintable.sol";
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
-
-
-
 // TODO define a solutions struct that can hold an index & an address
-
 
 // TODO define an array of the above struct
 
-
 // TODO define a mapping to store unique solutions submitted
-
-
 
 // TODO Create an event to emit when a solution is added
 
-
-
 // TODO Create a function to add the solutions to the array and emit the event
-
-
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
+contract SolnSquareVerifier is CustomERC721Token {
+    struct Solution{
+        uint256 index;
+        address addr;
+    }
 
-  
+    Solution[] public solutions;
+    mapping(bool=> Solution) submittedSolutions;
+    event addSolutionEvent(address caller, uint256 index);
 
+    function addSolution(uint256 index, address addr) public returns(uint arraylength){
+        Solution memory newSolution;
+        newSolution.index = index;
+        newSolution.addr = addr;
+        solutions.push(newSolution);
+        emit addSolutionEvent(addr, index);
+        return solutions.length;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // function mintToken(address to, uint256 tokenId, Solution memory solution) public{
+    //     require(submittedSolutions[solution] != true, "solution is submitted before");
+    //     mint(to, tokenId);
+    //     submittedSolutions[true] = solution;
+    // }
+}
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,34 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+var SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+
+let account_one;
+let account_two;
+
+contract('SolnSquareVerifier', accounts => {
+     account_one = accounts[0];
+     account_two = accounts[1];
+
+});
+
+describe('match SolnSquareVerifier spec', function () {
+    beforeEach(async function () {
+    
+        this.contract = await SolnSquareVerifier.new({ from: account_one });
+        // console.log("this.contract", myContract)
+        // TODO: mint multiple tokens
+        // let tokenId1 = 1;
+        // let tokenId2 = 2
+        // await this.contract.mint(account_one, tokenId1, { from: account_one })
+        // await this.contract.mint(account_two, tokenId2, { from: account_one })
+    })
+
+    it('add new solution', async function () {
+        const arrLength = await this.contract.addSolution(1, account_one);
+        // const sol = await this.contract.solutions();
+        // console.log("arrLength",sol)
+        // assert.equal(totalSupply, 2)
+    })
+});
+
